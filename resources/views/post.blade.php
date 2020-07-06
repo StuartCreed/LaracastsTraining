3a75fb75bfa2d05a49c97d234894f765fe2b8e63
Stuart Creed <stuart.a.creed@gmail.com>
2020-07-06 17:33:32 +0100

    Episode 7 complete

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Laravel</title>
+    </head>
+    <body>
+      <div >
+        POST PAGE
+      </div>
+      <a href="http://127.0.0.1:8000/">Link back to home</a>
+      <div>
+        {{$post}}
+      </div>
+    </body>
+
+</html>
